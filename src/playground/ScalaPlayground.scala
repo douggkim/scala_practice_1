70aae2f0f38b390bc85c4fbcd72b43dc906607d4
DougKimBain <doukim@coca-cola.com>
2024-10-12 15:12:40 -0700

    first day: begin the repo

---
+++ src/playground/ScalaPlayground.scala
@@ -0,0 +1,15 @@
+package playground
+
+
+// extends App allows the object to be runnable right away
+object ScalaPlayground extends App {
+  println("Hello, Scala!")
+}
+
+
+// originally, compiler looks for `main`
+//object ScalaPlayground {
+//  def main(args: Array[String]): Unit = {
+//    println("Hello, Scala!")
+//  }
+//}
